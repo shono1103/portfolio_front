1ed5e639d79137a23722a075de98578597d6377b
shonoshono <shono131103@gmail.com>
2025-11-09 09:58:14 +0900

    ロゴを自分用に変更

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,100 +1,100 @@
 import './index.scss'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import {
-  faBriefcase,
-  faEnvelope,
-  faHome,
-  faLaptopCode,
-  faScrewdriverWrench,
-  faUser,
+	faBriefcase,
+	faEnvelope,
+	faHome,
+	faLaptopCode,
+	faScrewdriverWrench,
+	faUser,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link, NavLink } from 'react-router-dom'
 
 import codechefLogo from '../../assets/images/codechef.png'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/Sudip_logo_white.png'
+import LogoSubtitle from '../../assets/images/Shono_logo_white.png'
 
 const Sidebar = () => {
-  return (
-    <>
-      <div className="nav-bar">
-        <Link className="logo" to="/">
-          <img src={LogoS} alt="Logo" />
-          <img className="sub-logo" src={LogoSubtitle} alt="sudip" />
-        </Link>
-        <nav>
-          <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-          </NavLink>
-          <NavLink activeclassname="active" className="about-link" to="/about">
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-          </NavLink>
+	return (
+		<>
+			<div className="nav-bar">
+				<Link className="logo" to="/">
+					<img src={LogoS} alt="Logo" />
+					<img className="sub-logo" src={LogoSubtitle} alt="shono" />
+				</Link>
+				<nav>
+					<NavLink exact="true" activeclassname="active" to="/">
+						<FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+					</NavLink>
+					<NavLink activeclassname="active" className="about-link" to="/about">
+						<FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+					</NavLink>
 
-          <NavLink
-            activeclassname="active"
-            className="experience-link"
-            to="/experience"
-          >
-            <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
-          </NavLink>
+					<NavLink
+						activeclassname="active"
+						className="experience-link"
+						to="/experience"
+					>
+						<FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
+					</NavLink>
 
-          <NavLink
-            activeclassname="active"
-            className="projects-link"
-            to="/projects"
-          >
-            <FontAwesomeIcon icon={faLaptopCode} color="#4d4d4e" />
-          </NavLink>
+					<NavLink
+						activeclassname="active"
+						className="projects-link"
+						to="/projects"
+					>
+						<FontAwesomeIcon icon={faLaptopCode} color="#4d4d4e" />
+					</NavLink>
 
-          <NavLink
-            activeclassname="active"
-            className="skills-link"
-            to="/skills"
-          >
-            <FontAwesomeIcon icon={faScrewdriverWrench} color="#4d4d4e" />
-          </NavLink>
+					<NavLink
+						activeclassname="active"
+						className="skills-link"
+						to="/skills"
+					>
+						<FontAwesomeIcon icon={faScrewdriverWrench} color="#4d4d4e" />
+					</NavLink>
 
-          <NavLink
-            activeclassname="active"
-            className="contact-link"
-            to="/contact"
-          >
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-          </NavLink>
-        </nav>
-        <ul>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/banerjee-sudip/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/metal-oopa"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} color="#b9b9b9" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.codechef.com/users/metal_oopa"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={codechefLogo} width="24" alt="Codechef" />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
-  )
+					<NavLink
+						activeclassname="active"
+						className="contact-link"
+						to="/contact"
+					>
+						<FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+					</NavLink>
+				</nav>
+				<ul>
+					<li>
+						<a
+							href="https://www.linkedin.com/in/banerjee-sudip/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://github.com/metal-oopa"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={faGithub} color="#b9b9b9" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.codechef.com/users/metal_oopa"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img src={codechefLogo} width="24" alt="Codechef" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</>
+	)
 }
 
 export default Sidebar
